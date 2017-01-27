f49ff5d2f021eddae27d9332144f1dd2ccff1487
Waldemar M <KrypTheBear@users.noreply.github.com>
2017-01-27 20:36:31 +0100

    Added documentation, changed functionality

--- Finished/inichecker.py
+++ Finished/inichecker.py
@@ -1,15 +1,17 @@
 import glob, re, os
-# Glob in order to detect all *.ini files
-# OS to get the current workingdir
-# re for our most beloved regex
 
-def inichecker(varvalue):
+'''
+@brief Searches through current working directory and subfolders for files with a given variable, prints new file with results
+@param var Variable to be searched for
+@param fileextension Fileextension(Format ".fileextension") of the files to be search for (can be wildcard .*)
+'''
+def inichecker(var, fileextension):
     workingdir = os.getcwd()
     with open("vars.txt", "w") as text:
-        for file in glob.glob("%s/**/*.ini" % workingdir, recursive=True):
+        for file in glob.glob("%s/**/*"+fileextension % workingdir, recursive=True):
             with open(file, "r+") as check:
                 text.write("%s : \n" % file)
-                m = re.findall("^([^\W]+(?==\s*"+re.escape(varvalue)+"))", check.read(), re.M)
+                m = re.findall("^([^\W]+(?==\s*"+re.escape(var)+"))", check.read(), re.M)
                 for part in m:
                     text.write("%s\n" % part)
                 text.write("\n")
